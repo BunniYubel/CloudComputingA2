29ce49e0d3e8f021d37c1ab132889f81bec98df1
Chiao03 <84591143+Chiao03@users.noreply.github.com>
2024-05-15 21:17:39 +1000

    Update preprocess_income.py

--- Data/Income_Distribution/preprocess_income.py
+++ Data/Income_Distribution/preprocess_income.py
@@ -5,9 +5,15 @@ file_path = "/Users/YangWei-Chiao/CloudComputingA2/Data/Income_Distribution/abs_
 with open(file_path) as file:
     data = json.load(file)
 features = data["features"]
+preprocess_info = [feature["properties"] for feature in features]
 
 #lga code for NSW starts with 1, VIC starts with 2
 filtered_NSW_VIC_data = [feature for feature in features if feature["properties"]["lga_code"].startswith(("1", "2"))]
+preprocess_filtered_NSW_VIC_data = [feature["properties"] for feature in filtered_NSW_VIC_data]
 # print(filtered_NSW_VIC_data)
+
+with open('all_states_income.json', 'w') as file:
+    json.dump(preprocess_info, file)
+
 with open('filtered_NSW_VIC_income.json', 'w') as file:
-    json.dump(filtered_NSW_VIC_data, file)
+    json.dump(preprocess_filtered_NSW_VIC_data, file)
